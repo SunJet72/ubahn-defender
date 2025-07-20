f0ad14392b2149b60a0b660562249464d1fabdcf
pawel <pawelkancl@gmail.com>
2025-07-20 22:57:56 +0200

    added money to the train from the prev station

--- Assets/Scripts/db_mvg/DbManager.cs
+++ Assets/Scripts/db_mvg/DbManager.cs
@@ -91,7 +91,8 @@ public class DbManager
     public async Task enter_the_train(string from, string to)
     {
         await WaitUntilReady();
-        Conn.Reducers.EnterTheTrain(from + to, from, to, player_id, 1000);
+        uint station_from_money = Conn.Db.Station.MvgId.Find(from).Money;
+        Conn.Reducers.EnterTheTrain(from + to, from, to, player_id, station_from_money);
     }
 
     public async Task leave_the_train(string from, string to)
